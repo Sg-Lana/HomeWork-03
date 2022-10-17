a426f45fbd805309fe5a957fa34ef81135802ae8
svetlana <nooch@yandex.ru>
2022-10-17 22:23:39 +0300

    03

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿/* напишите программу которая задает массив из 8 случайных элементов и выдает 
+его на экран*/
+
+int[] NewArray(int len = 8)
+{
+    int[] array = new int[len];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0,100); 
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]} ");
+    }
+}
+int[] arrey = NewArray(); 
+PrintArray(arrey);
